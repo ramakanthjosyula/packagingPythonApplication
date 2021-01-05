3aa42c510c3ee9fb3ec4912830ad563dca024e29
rjosyula <ramakanth.josyula@commscope.com>
2021-01-05 00:04:41 -0800

    Format as per pep8

--- helloWorld.py
+++ helloWorld.py
@@ -3,9 +3,11 @@ import requests
 
 URL = 'https://en.wikipedia.org/wiki/"Hello,_World!"_program'
 
+
 def do_hello(url):
     result = requests.get(url)
-    return (re.findall("<title>(.*?)</title>",result.text)[0])
+    return re.findall("<title>(.*?)</title>", result.text)[0]
 
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
     print(do_hello(URL))
